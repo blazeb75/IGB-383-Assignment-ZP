8967b17768032ec1ac378e8b024b8f113bad487e
Zac Paternoster <blazeb75@gmail.com>
2020-03-08 12:32:36 +1000

    Project template

---
+++ Assets/NavigationAgent.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class NavigationAgent : MonoBehaviour {
+
+    //Navigation Variables
+    public WaypointGraph graphNodes;
+    public List<int> openList = new List<int>();
+    public List<int> closedList = new List<int>();
+    public List<int> currentPath = new List<int>();
+    public List<int> greedyPaintList = new List<int>();
+    public int currentPathIndex = 0;
+    public int currentNodeIndex = 0;
+
+    // Use this for initialization
+    void Start () {
+        //Find waypoint graph
+        graphNodes = GameObject.FindGameObjectWithTag("waypoint graph").GetComponent<WaypointGraph>();
+
+        //Initial node index to move to
+        currentPath.Add(currentNodeIndex);
+    }
+
+    //A-Star Search
+    public List<int> AStarSearch(int start, int goal) {
+        
+        //Code here
+
+        return null;
+    }
+
+    //Greedy Search
+    public List<int> GreedySearch(int currentNode, int goal, List<int> path) {
+
+        //Code here
+
+        return path;
+    }
+}
